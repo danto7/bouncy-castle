05ec9aa14b74822f596d36ee1e107d62ba7cd412
Daniel Jensen <4144311+danto7@users.noreply.github.com>
2023-05-28 18:55:59 +0200

    add kubernetes

---
+++ longhorn.tf
@@ -0,0 +1,7 @@
+resource "helm_release" "longhorn" {
+  name       = "longhorn"
+  repository = "https://charts.longhorn.io"
+  chart      = "longhorn"
+  version    = "1.4.2"
+  namespace  = "longhorn-system"
+}
--- provider.tf
+++ provider.tf
@@ -4,7 +4,19 @@ terraform {
       source  = "auth0/auth0"
       version = "0.47.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.20.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "2.9.0"
+    }
   }
 }
 
 provider "auth0" {}
+
+provider "kubernetes" {}
+
+provider "helm" {}

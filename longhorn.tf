399b2d9afb866d3b36b91b549fca6b508f558238
Daniel Jensen <4144311+danto7@users.noreply.github.com>
2023-05-28 19:31:42 +0200

    create namespace

--- longhorn.tf
+++ longhorn.tf
@@ -1,7 +1,12 @@
+resource "kubernetes_namespace" "longhorn" {
+  metadata {
+    name = "longhorn-system"
+  }
+}
 resource "helm_release" "longhorn" {
   name       = "longhorn"
   repository = "https://charts.longhorn.io"
   chart      = "longhorn"
   version    = "1.4.2"
-  namespace  = "longhorn-system"
+  namespace  = kubernetes_namespace.longhorn.metadata[0].name
 }

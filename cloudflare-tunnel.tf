8cc80f7d807939a9438a0281ae7fdf5610d4f471
Daniel Jensen <4144311+danto7@users.noreply.github.com>
2023-10-31 21:22:57 +0100

    update tunnel name

--- cloudflare-tunnel.tf
+++ cloudflare-tunnel.tf
@@ -6,7 +6,7 @@ resource "random_password" "tunnel_secret" {
   length = 32
 }
 
-resource "cloudflare_tunnel" "example" {
+resource "cloudflare_tunnel" "olymp" {
   account_id = data.cloudflare_accounts.d-jensen_de.id
   name       = "olymp"
   secret     = base64encode(random_password.tunnel_secret.result)

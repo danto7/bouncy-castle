108afada22190f99d09fdce16e1d704efdc50330
Daniel Jensen <4144311+danto7@users.noreply.github.com>
2023-10-31 20:13:28 +0100

    update longhorn

---
+++ longhorn.tf
@@ -0,0 +1,32 @@
+resource "kubernetes_namespace" "longhorn" {
+  metadata {
+    name = "longhorn-system"
+  }
+}
+resource "helm_release" "longhorn" {
+  name       = "longhorn"
+  repository = "https://charts.longhorn.io"
+  chart      = "longhorn"
+  version    = "1.5.1"
+  namespace  = kubernetes_namespace.longhorn.metadata[0].name
+
+  values = [yamlencode({
+    csi = {
+      attacherReplicaCount    = 1
+      provisionerReplicaCount = 1
+      resizerReplicaCount     = 1
+      snapshotterReplicaCount = 1
+    }
+    longhornUI = {
+      replicas = 1
+    }
+    persistence = {
+      defaultDataLocality : "best-effort"
+      defaultClassReplicaCount : 2
+    }
+    defaultSettings = {
+      backupTarget = "nfs://192.168.188.7:/mnt/ravenclaw/olymp-backup"
+    }
+  })]
+}
+

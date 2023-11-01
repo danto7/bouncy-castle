4037eab46ae0176cd272e5eaa08b5266bc2043d6
Daniel Jensen <4144311+danto7@users.noreply.github.com>
2023-11-01 18:38:31 +0100

    deploy grafana

---
+++ grafana.tf
@@ -0,0 +1,140 @@
+locals {
+  grafana_tag = "10.2.0"
+}
+
+resource "kubernetes_namespace" "grafana" {
+  metadata {
+    name = "grafana"
+  }
+}
+
+resource "kubernetes_persistent_volume_claim" "grafana" {
+  metadata {
+    name      = "grafana-data"
+    namespace = kubernetes_namespace.grafana.metadata[0].name
+  }
+
+  spec {
+    access_modes = ["ReadWriteOnce"]
+
+    resources {
+      requests = {
+        storage = "1Gi"
+      }
+    }
+  }
+}
+
+resource "kubernetes_deployment" "grafana" {
+  metadata {
+    name      = "grafana"
+    namespace = kubernetes_namespace.grafana.metadata[0].name
+
+    labels = {
+      app = "grafana"
+    }
+  }
+
+  spec {
+    selector {
+      match_labels = {
+        app = "grafana"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "grafana"
+        }
+      }
+
+      spec {
+        volume {
+          name = "data"
+
+          persistent_volume_claim {
+            claim_name = kubernetes_persistent_volume_claim.grafana.metadata[0].name
+          }
+        }
+
+        container {
+          name  = "grafana"
+          image = "grafana/grafana:${local.grafana_tag}"
+
+          port {
+            name           = "http-grafana"
+            container_port = 3000
+            protocol       = "TCP"
+          }
+
+          resources {
+            requests = {
+              cpu = "250m"
+
+              memory = "750Mi"
+            }
+          }
+
+          volume_mount {
+            name       = "data"
+            mount_path = "/var/lib/grafana"
+          }
+
+          liveness_probe {
+            tcp_socket {
+              port = "3000"
+            }
+
+            initial_delay_seconds = 30
+            timeout_seconds       = 1
+            period_seconds        = 10
+            success_threshold     = 1
+            failure_threshold     = 3
+          }
+
+          readiness_probe {
+            http_get {
+              path   = "/robots.txt"
+              port   = "3000"
+              scheme = "HTTP"
+            }
+
+            initial_delay_seconds = 10
+            timeout_seconds       = 2
+            period_seconds        = 30
+            success_threshold     = 1
+            failure_threshold     = 3
+          }
+
+          image_pull_policy = "IfNotPresent"
+        }
+
+        security_context {
+          supplemental_groups = [0]
+          fs_group            = 472
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "grafana" {
+  metadata {
+    name      = "grafana"
+    namespace = kubernetes_namespace.grafana.metadata[0].name
+  }
+
+  spec {
+    port {
+      protocol    = "TCP"
+      port        = 3000
+      target_port = "http-grafana"
+    }
+
+    selector = {
+      app = "grafana"
+    }
+  }
+}
+
